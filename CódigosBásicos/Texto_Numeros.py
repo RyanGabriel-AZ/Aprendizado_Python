c948537f5279d7bf78c9a5509707d433c5d19569
RyanGabriel-AZ <rgac@poli.br>
2026-04-09 19:47:11 -0300

    Amanha termino

--- CódigosBásicos/Texto_Numeros.py
+++ CódigosBásicos/Texto_Numeros.py
@@ -23,3 +23,33 @@ print(" meu nome \n quebrou página \t dei tab \\ isso é uma barra \' isso é u
 print(" Aqui temos abc agora temos abc\b, eu acabei de dar um backspace")
 print("ABCrepetiu\rDEF")# ele volta pro inicio da frase e substitui ABC por DEF
 
+
+#ok agora vamos usar o numeros
+# NUmeros podem ser atribuidos a variaveis, podem ser pontos flutuantes, ter expressões, etc
+
+# temos operadores: + soma, - subtração, 
+# * multiplicação, ** potencia, / divisão, // divisão inteira, % resto
+# Podemos colocar eles em listas também
+x = 3.1415926535
+print(round(x, 2)) # aqui por exemplo uma aritmetrica de ponto flutuante, 
+#A função round é muito importante, pois ela vai arredondar casas  pós virgula.
+# Ele recebe X, e nesse caso, só vai mostrar 2 casas pós a virgula
+#Podemos também converter um numero em string
+y = str(x) #formatei para string
+print(y)
+
+dois = 2
+Um = 1
+resultado = dois + Um #expressão com variaveis
+print(resultado)
+
+#Ok vamos agora para listas
+teste = "012345678"
+print( teste[0]) # o colchetes retorna a posição
+print(teste[4: 7])
+print(teste[4: ])
+print(teste[: 4])
+#vamos entender assim para ficar
+#melhor, teste[ A partir daqui será incluido: A partir daqui será excluido]
+#Isso de forma leiga, claro. Pois sabemos que isso é a cauda: cabeça da lista.
+
